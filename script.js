c204103853d2939bfb14f551b24be41f5aea8752
Jon Bjarke Sørensen <jonbjarke25@outlook.dk>
2022-09-13 12:19:36 +0200

    "jon ændringer 2"

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 for(let i = 0; i < 4; i++) {
     console.log('Hallo world!');
 }
-<<<<<<< HEAD
 // komentar test J // 
-=======
 
 // Dalena 
->>>>>>> 6ae45df1b0f258b3b7dc6e409156f276a13e61b7
 for (let i = 0; i < 4; i++) {
   console.log("Hallo world!");
   console.log("Marc test");
