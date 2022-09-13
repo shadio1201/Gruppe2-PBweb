f242a75fbbce3fde7d3a37616683cb5a4f06bb7f
shadio1201 <lkr97km@hotmail.dk>
2022-09-13 12:17:45 +0200

    index with script

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 for(let i = 0; i < 4; i++) {
     console.log('Hallo world!');
 }
-<<<<<<< HEAD
 // komentar test J // 
-=======
 
 window.addEventListener("mousemove", ()=> {
     console.log('wow!');
 })
 // Dalena 
->>>>>>> 6ae45df1b0f258b3b7dc6e409156f276a13e61b7
 for (let i = 0; i < 4; i++) {
   console.log("Hallo world!");
   console.log("Marc test");
